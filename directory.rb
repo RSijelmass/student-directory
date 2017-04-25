8b24aee2c14fbe65436211a218741fd8de425c62
RSijelmass <R.Sijelmass@gmail.com>
2017-04-25 21:34:52 +0100

    Add methods to print specific letter, short names, and with indices

--- directory.rb
+++ directory.rb
@@ -1,30 +1,55 @@
-#all students in array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students;"
+  puts "to finish, just hit return twice."
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students!"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-----------"
 end
 
 def print(students)
-  students.each{ |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_index(students)
+    i = 0
+    while i < students.length
+      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+      i += 1
+  end
+end
+
+def print_letter(students, letter)
+  students.each_with_index do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == letter
+  end
+end
+
+def print_short(students)
+  students.each_with_index do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 11
+  end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
-print(students)
+print_index(students)
+#print_letter(students, "H")
+#print_short(students)
 print_footer(students)
