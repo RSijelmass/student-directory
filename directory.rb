da8940dc6bdc627a194dcaedc6c72a9c4af09803
Rita Sijelmass <R.Sijelmass@gmail.com>
2017-04-27 11:57:33 +0100

    Refactor directory.db by adding method process

--- directory.rb
+++ directory.rb
@@ -1,21 +1,8 @@
 def interactive_menu
   @students ||= []
   loop do
-    #1. print the menu and ask what to do
     print_menu
-    #2. read the input and save it in a variable
-    selection = gets.chomp
-    #3. do what the user has asked
-    case selection
-      when "1"
-        input_students
-      when "2"
-        show_students
-      when "9"
-        exit
-      else
-        puts "I don't fully understand, try again."
-    end
+    process(gets.chomp)
   end
 end
 
@@ -25,13 +12,16 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-  if @students != []
-    print_header
-    print_students
-    print_footer
-  else
-    puts "There are no students to show"
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't fully understand, try again."
   end
 end
 
@@ -52,12 +42,22 @@ def input_students
   end
 end
 
+def show_students
+  if @students != []
+    print_header
+    print_students_list
+    print_footer
+  else
+    puts "There are no students to show"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy".center(50)
   puts "-----------".center(50)
 end
 
-def print_students
+def print_students_list
    @students.each do |student|
      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
    end
